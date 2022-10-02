4ab436681d8b983e464bb093ce0afdffaf1b5e61
depdiller <stanislavoronov@yandex.ru>
2022-10-02 13:43:50 +0300

    Add functions: invert and key_gen

---
+++ cipher.py
@@ -0,0 +1,19 @@
+from array import array
+from copy import copy
+
+
+def invert(data: bytearray) -> bytearray:
+    result = data[:]
+    for index in range(len(result)):
+        result[index] ^= 0xFF
+        return result
+
+def key_gen(key_zero: bytearray) -> list[bytearray]:
+    key_list = []
+    key_one = invert(key_zero)
+    key_two = bytes(a ^ b for a, b in zip(key_zero, key_one))
+    
+    key_list.append(key_zero)
+    key_list.append(key_one)
+    key_list.append(key_two)
+    return key_list
